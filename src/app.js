d3e758f61e725e8d8fa8535a78c04c7827140719
Vipul <vjaiswal.2810@gmail.com>
2024-10-13 19:09:41 +0530

    delete and patch

--- src/app.js
+++ src/app.js
@@ -19,16 +19,54 @@ app.post("/signup", async (req, res) => {
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
   try {
-   const user =  await User.find({
+    const user = await User.find({
       emailId: userEmail,
     });
-    res.send(user);
+
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);   
+    }
   } catch (err) {
     res.send(400).send("something went wrong");
   }
 });
 
-app.get("/feed", (req, res) => {});
+app.get("/feed", async (req, res) => {
+    try{
+        const users=await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
+
+app.delete("/user", async(req,res)=>{
+    const userId= req.body.userId;
+    try{
+        const user=await User.findByIdAndDelete(userId);
+        res.send(user+"Deleted successfully");
+    }catch(err){
+        res.staus(404).send("Error occured");
+    }
+})
+
+
+app.patch("/user", async (req,res)=>{
+    const userId= req.body.userId;
+    const data= req.body;
+    try{
+        const user =await User.findByIdAndUpdate({_id: userId}, data, {
+            returnDocument:"after"
+        })
+        console.log(user)
+        res.send("User data updated successfully");
+    }catch(err){
+        res.staus(404).send("Error occured");
+    }
+})
 
 connectDB()
   .then(() => {
