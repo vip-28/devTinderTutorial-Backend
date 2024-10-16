503b3fdd2793eb745265feafaa07f1faecdb6071
Vipul <vjaiswal.2810@gmail.com>
2024-10-16 19:11:59 +0530

    validations and sanitizations

--- src/app.js
+++ src/app.js
@@ -46,7 +46,9 @@ app.get("/feed", async (req, res) => {
 app.delete("/user", async(req,res)=>{
     const userId= req.body.userId;
     try{
-        const user=await User.findByIdAndDelete(userId);
+        const user=await User.findByIdAndDelete(userId,{
+            runValidators:true,
+        });
         res.send(user+"Deleted successfully");
     }catch(err){
         res.staus(404).send("Error occured");
@@ -54,14 +56,46 @@ app.delete("/user", async(req,res)=>{
 })
 
 
-app.patch("/user", async (req,res)=>{
-    const userId= req.body.userId;
+app.patch("/user/:userId", async (req,res)=>{
+    const userId= req?.params?.userId;
     const data= req.body;
+
+
     try{
+        const ALLOWED_UPDATES=[
+            "userId","photoUrl","about","gender","age","skills","password","emailId"
+        ]
+
+    
+    
+    const isUpdateAllowed= Object.keys(data).every((k)=>
+    ALLOWED_UPDATES.includes(k)
+    )
+    if(data?.skills.length>10){
+        throw new Error("Array Length exceeded");
+    }
+    if(!isUpdateAllowed){
+       throw new Error("Update not allowed because of Bad Change")
+    }
         const user =await User.findByIdAndUpdate({_id: userId}, data, {
-            returnDocument:"after"
+            returnDocument:"after",
+            runValidators:true,
         })
         console.log(user)
+        res.send("User data updated successfully");
+    }catch(err){
+        res.status(404).send("Error occured "+err.message);
+    }
+})
+
+
+app.patch("/userEmail", async (req,res)=>{
+    const emailId= req.body.emailId;
+    const data= req.body;
+    try{
+        const user= await User.findOneAndUpdate({emailId: emailId},data,{
+            runValidators:true,
+        })
         res.send("User data updated successfully");
     }catch(err){
         res.staus(404).send("Error occured");
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,67 @@
 const mongoose = require("mongoose");
+const validator= require("validator");
 
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required:true,
+    minLength: 3,
   },
   lastName: {
     type: String,
+
   },
   emailId: {
     type: String,
+    required:true,
+    unique:true, 
+    trim:true,
+    validate(value){
+        if(!validator.isEmail(value)){
+            throw new Error("Invalid Email Address: "+ value);
+        }
+    }   
+
   },
   password: {
     type: String,
+    required:true,
+    validate(value){
+        if(!validator.isStrongPassword(value)){
+            throw new Error("Password is Weak: "+ value);
+        }
+    }
+
   },
   age: {
     type: Number,
+    min:18,
   },
   gender: {
     type: String,
+    validate(value){
+        if(!["male","female","others"].includes(value)){
+            throw new Error("Gender Data is not valid");
+        }
+    },
   },
+  photoUrl:{
+    type:String,
+    validate(value){
+        if(!validator.isURL(value)){
+            throw new Error("Invalid Photo URL: "+ value);
+        }
+    }
+  },
+  about:{
+    type:String,
+    default:"Hello! i am new to the Platform."
+  },
+  skills:{
+    type:[String],
+  }
+},{
+    timestamps:true,
 });
 
 const User = mongoose.model("User", userSchema);
